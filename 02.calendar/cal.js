ec9dc82acaef03570ffd5a765424c9a6c3ba0099
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-09-02 18:37:05 +0900

    カレンダープログラム作成

---
+++ 02.calendar/cal.js
@@ -0,0 +1,29 @@
+import minimist from "minimist";
+import dayjs from "dayjs";
+import ja from "dayjs/locale/ja.js";
+
+dayjs.locale(ja);
+
+var argv = minimist(process.argv.slice(2));
+
+const year = argv["y"] || dayjs().format("YYYY");
+const month = argv["m"] || dayjs().format("M");
+const weeks = ["日", "月", "火", "水", "木", "金", "土"];
+
+const lastDay = dayjs(new Date(year, month, 0));
+console.log(lastDay.date());
+
+// 1日の曜日を 0-6 で返す、日曜日は1、土曜日は0
+let startWeekDay = dayjs(new Date(year, month, -1, 1));
+console.log(startWeekDay.day());
+
+console.log(`${month}月 ${year}`.padStart(13));
+console.log(weeks.join(" "));
+process.stdout.write("  ".repeat(startWeekDay.day()));
+
+for (let i = 1; i <= lastDay.date(); i++) {
+  process.stdout.write(` ${i} `);
+  if (startWeekDay.date(i).day() === 6 || i === lastDay.date()) {
+    process.stdout.write("\n");
+  }
+}

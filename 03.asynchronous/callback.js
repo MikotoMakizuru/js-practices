68586695fc60abc66483b20f4c4c858ba323e135
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-09-10 01:02:23 +0900

    callback:エラーなしのプログラム作成

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,17 +1,18 @@
 import timers from "timers/promises";
 import sqlite3 from 'sqlite3';
-const db = new sqlite3.Database(':book');
 
+var db = new sqlite3.Database(':memory:');
 db.run("CREATE TABLE IF NOT EXISTS books (id integer primary key autoincrement, title text not null unique)");
+
 await timers.setTimeout(100);
-const title = db.prepare("INSERT INTO books(title) VALUES (?)");
+var title = db.prepare("INSERT INTO books(title) VALUES (?)");
 for (let i = 0; i < 5; i++) {
   title.run("タイトル" + i);
 }
 title.finalize();
+
 await timers.setTimeout(100);
-db.each("SELECT * FROM books", (row) => {
+db.each("SELECT * FROM books", (err, row) => {
   console.log(`${row.id} ${row.title}`);
 });
-
 db.close();

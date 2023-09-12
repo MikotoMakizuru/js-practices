2a5735fe9c8c56cc215842ccdd9798da531e6b61
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-09-12 23:41:08 +0900

    日にち表示箇所の間隔調整
    startWeekDayが持つ値修正

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -11,16 +11,16 @@ const month = argv["m"] || dayjs().format("M");
 const weeks = ["日", "月", "火", "水", "木", "金", "土"];
 const lastDay = dayjs(new Date(year, month, 0));
 
-// 1日の曜日を 0-6 で返す、日曜日は1、土曜日は0
-let startWeekDay = dayjs(new Date(year, month, -1, 1));
+let startWeekDay = dayjs(new Date(year, month - 1, 1));
 
 console.log(`${month}月 ${year}`.padStart(13));
 console.log(weeks.join(" "));
 process.stdout.write("   ".repeat(startWeekDay.day()));
 
 for (let i = 1; i <= lastDay.date(); i++) {
-  process.stdout.write(`${i}`.padStart(3));
+  process.stdout.write(`${i} `.padStart(3));
   if (startWeekDay.date(i).day() === 6 || i === lastDay.date()) {
     process.stdout.write("\n");
   }
 }
+process.stdout.write("\n");

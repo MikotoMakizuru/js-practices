5c1d0e0a185e1c55409f90dff7490a09424dc150
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-09-17 03:37:14 +0900

    不要なカンマ削除

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -3,7 +3,7 @@ import sqlite3 from "sqlite3";
 
 var db = new sqlite3.Database(":memory:");
 db.run(
-  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
 );
 
 await timers.setTimeout(100);

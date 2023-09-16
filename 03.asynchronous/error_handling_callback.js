3fc83c7118de6008f57f6a709d02635f1a2e3772
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-09-17 03:27:49 +0900

    コールバック形式のエラーハンドリングを実装したプログラム作成

---
+++ 03.asynchronous/error_handling_callback.js
@@ -0,0 +1,29 @@
+import timers from "timers/promises";
+import sqlite3 from "sqlite3";
+
+var db = new sqlite3.Database(":memory:");
+db.run(
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+);
+
+await timers.setTimeout(100);
+var title = db.prepare("INSERT INTO books(title) VALUES (?)");
+for (let i = 0; i < 5; i++) {
+  title.run("タイトル" + i, function (err) {
+    if (err) {
+      console.log("エラーが発生しました");
+    }
+  });
+}
+title.finalize();
+
+await timers.setTimeout(100);
+db.each("SELECT * FROM books", (err, row) => {
+  if (err) {
+    console.log("エラーが発生しました");
+  } else {
+    console.log(`${row.id} ${row.title}`);
+  }
+});
+
+db.close();

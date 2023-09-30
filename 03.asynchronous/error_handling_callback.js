fb04e5c68436f397c6a8c5f73d84de9fc56e014b
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-09-30 20:07:15 +0900

    エラーハンドリングありコールバック形式プログラム作成

--- 03.asynchronous/error_handling_callback.js
+++ 03.asynchronous/error_handling_callback.js
@@ -1,30 +1,43 @@
 import timers from "timers/promises";
 import sqlite3 from "sqlite3";
 
-var db = new sqlite3.Database(":memory:");
-db.run(
-  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
-);
+const db = new sqlite3.Database(":memory:");
 
-await timers.setTimeout(100);
-var title = db.prepare("INSERT INTO books(title) VALUES (?)");
-for (let i = 0; i < 5; i++) {
-  title.run("タイトル" + i, function (err) {
-    if (err) {
-      console.log("エラーが発生しました");
+const createTable = () => {
+  db.run(
+    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  );
+};
+
+const insertRecord = () => {
+  var title = db.prepare("INSERT INTO books(title) VALUES (?)");
+  for (let i = 0; i < 5; i++) {
+    title.run("タイトル" + i, (error) => {
+      if (error) {
+        console.log("レコード追加に失敗しました");
+      }
+    });
+  }
+};
+
+const outputDisplayTitle = () => {
+  db.each("SELECT * FROM books", (error, row) => {
+    if (error) {
+      console.log("本のタイトル表示に失敗しました");
+    } else {
+      console.log(`${row.id} ${row.title}`);
     }
   });
-}
-title.finalize();
+};
 
-await timers.setTimeout(100);
-db.each("SELECT * FROM books", (err, row) => {
-  if (err) {
-    console.log("エラーが発生しました");
-  } else {
-    console.log(`${row.id} ${row.title}`);
-  }
-});
+const deleteTable = () => {
+  db.run("DROP TABLE books");
+};
 
+createTable();
 await timers.setTimeout(100);
-db.run("DROP TABLE books");
+insertRecord();
+await timers.setTimeout(100);
+outputDisplayTitle();
+await timers.setTimeout(100);
+deleteTable();

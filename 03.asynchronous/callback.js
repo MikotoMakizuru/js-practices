a242207bc003377a3c2f514d59097547a3fccaab
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-10-19 01:46:04 +0900

    prettier --check指摘箇所修正

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -5,7 +5,7 @@ const db = new sqlite3.Database(":memory:");
 function createTable(callback) {
   db.run(
     "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    callback
+    callback,
   );
 }
 

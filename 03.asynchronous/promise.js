2244447be8f6e6667ec504108e02144225a62d86
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-10-29 23:17:34 +0900

    関数をモジュール化

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,58 +1,7 @@
-import sqlite3 from "sqlite3";
+import * as db_operation_function from "./function_module.js";
 
-const db = new sqlite3.Database(":memory:");
-
-function createTable() {
-  return new Promise((resolve) => {
-    db.run(
-      "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-      () => {
-        resolve();
-      }
-    );
-  });
-}
-
-function insertRecord() {
-  return new Promise((resolve, reject) => {
-    const title = db.prepare(
-      "INSERT INTO books (title) VALUES (?)",
-      function (error) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      }
-    );
-    // コールバック同様for文で書こうと思いましたが、5件出力できないので断念。
-    title.run("本のタイトル");
-  });
-}
-
-function outputTitleDisplay() {
-  return new Promise((resolve, reject) => {
-    const getDataSql = "SELECT * FROM books";
-    db.each(getDataSql, function (error, row) {
-      if (error) {
-        reject(error);
-      } else {
-        console.log(`${row.id} ${row.title}`);
-      }
-      resolve();
-    });
-  });
-}
-
-function deleteTable() {
-  return new Promise((resolve) => {
-    db.run("DROP TABLE books", () => {
-      resolve();
-    });
-  });
-}
-
-createTable()
-  .then(() => insertRecord())
-  .then(() => outputTitleDisplay())
-  .then(() => deleteTable());
+db_operation_function
+  .createTable()
+  .then(() => db_operation_function.insertRecord())
+  .then(() => db_operation_function.outputTitleDisplay())
+  .then(() => db_operation_function.deleteTable());

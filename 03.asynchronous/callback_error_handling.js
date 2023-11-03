757dd23f9f736e0652799163af78036f62c0a642
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-03 21:18:41 +0900

    独自関数の定義を削除

--- 03.asynchronous/callback_error_handling.js
+++ 03.asynchronous/callback_error_handling.js
@@ -1,45 +1,33 @@
 import sqlite3 from "sqlite3";
+import timers from "timers/promises";
 
 const db = new sqlite3.Database(":memory:");
 
-function createTable(callback) {
-  db.run(
-    "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-    callback,
-  );
-}
+db.run(
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+);
 
-function insertRecord(callback) {
-  const title = db.prepare("INSERT INTO books (title) VALUES (?)");
-  for (let i = 0; i < 5; i++) {
-    title.run("タイトル" + i, (error) => {
-      if (error) {
-        console.log("レコード追加失敗");
-      }
-    });
+await timers.setTimeout(100);
+const title = db.prepare("INSERT INTO books (title) VALUES (?)", (error) => {
+  if (error) {
+    console.log("レコード追加失敗");
   }
-  title.finalize(callback);
-}
-
-function outputTitleDisplay() {
-  const getDataSql = "SELECT * FROM books";
-  db.each(getDataSql, (error, row) => {
+});
+for (let i = 0; i < 5; i++) {
+  title.run("タイトル" + i, (error) => {
     if (error) {
-      console.log("本のタイトル表示失敗");
-    } else {
-      console.log(`${row.id} ${row.title}`);
+      console.log("レコード追加失敗");
     }
   });
 }
 
-function deleteTable() {
-  db.run("DROP TABLE books");
-}
-
-createTable(function () {
-  insertRecord(function () {
-    outputTitleDisplay(function () {
-      deleteTable(function () {});
-    });
-  });
+await timers.setTimeout(100);
+db.all("SELECT * FROM books", (error, rows) => {
+  if (error) {
+    console.log("本のタイトル表示失敗");
+  }
+  rows.forEach((row) => console.log(`${row.id} ${row.title}`));
 });
+
+await timers.setTimeout(100);
+db.run("DROP TABLE books");

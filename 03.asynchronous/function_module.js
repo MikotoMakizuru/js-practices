a02e850fce6ef050003ad2a9ff9c20f58b7f31fe
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-04 15:41:03 +0900

    node-sqlite3の関数をPromiseとして使える関数を定義

--- 03.asynchronous/function_module.js
+++ 03.asynchronous/function_module.js
@@ -3,64 +3,80 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 function createTable() {
-  return new Promise(() => {
+  return new Promise((resolve, reject) => {
     db.run(
-      "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+      "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+      function (error) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      },
     );
   });
 }
 
-function insertBookTitle() {
-  return new Promise(() => {
-    db.prepare("INSERT INTO books (title) VALUES (?)");
+function insertBookTitleZeroRuby() {
+  return new Promise((resolve, reject) => {
+    db.run(
+      "INSERT INTO books (title) VALUES (?)",
+      "ゼロからわかるRuby超入門",
+      function (error) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      },
+    );
   });
 }
 
-createTable().then(() => insertBookTitle);
+function insertBookTitleCherryBook() {
+  return new Promise((resolve, reject) => {
+    db.run(
+      "INSERT INTO books (title) VALUES (?)",
+      "プロを目指す人のためのRuby入門",
+      function (error) {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      },
+    );
+  });
+}
 
-// function createTable() {
-//   return new Promise((resolve) => {
-//     db.run(
-//       "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-//       () => resolve(),
-//     );
-//   });
-// }
+function outputTitleDisplay() {
+  return new Promise((resolve, reject) => {
+    db.each("SELECT * FROM books", function (error, row) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(console.log(`${row.id} ${row.title}`));
+      }
+    });
+  });
+}
 
-// function insertRecord() {
-//   return new Promise((resolve, reject) => {
-//     const title = db.prepare(
-//       "INSERT INTO books (title) VALUES (?)",
-//       function (error) {
-//         if (error) {
-//           reject(error);
-//         } else {
-//           resolve();
-//         }
-//       },
-//     );
-//     // コールバック同様for文で書こうと思いましたが、5件出力できないので断念。
-//     title.run("本のタイトル");
-//   });
-// }
+function deleteTable() {
+  return new Promise((resolve, reject) => {
+    db.run("DROP TABLE books", function (error) {
+      if (error) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
+  });
+}
 
-// function outputTitleDisplay() {
-//   return new Promise((resolve, reject) => {
-//     const getDataSql = "SELECT * FROM books";
-//     db.each(getDataSql, function (error, row) {
-//       if (error) {
-//         reject(error);
-//       } else {
-//         resolve(console.log(`${row.id} ${row.title}`));
-//       }
-//     });
-//   });
-// }
-
-// function deleteTable() {
-//   return new Promise((resolve) => {
-//     db.run("DROP TABLE books", () => resolve());
-//   });
-// }
-
-// export { createTable, insertRecord, outputTitleDisplay, deleteTable };
+export {
+  createTable,
+  insertBookTitleZeroRuby,
+  insertBookTitleCherryBook,
+  outputTitleDisplay,
+  deleteTable,
+};

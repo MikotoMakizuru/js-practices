2b358b68fe65a72925100249540edd164b4de4ec
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-07 01:01:02 +0900

    node-sqlite3の関数を使ったコールバック形式に修正

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -1,25 +1,27 @@
 import sqlite3 from "sqlite3";
-import timers from "timers/promises";
 
 const db = new sqlite3.Database(":memory:");
 
 db.run(
   "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run(
+      "INSERT INTO books (title) VALUES (?)",
+      "ゼロからわかるRuby超入門",
+      () => {
+        db.run(
+          "INSERT INTO books (title) VALUES (?)",
+          "プロを目指す人のためのRuby入門",
+          () => {
+            db.all("SELECT * FROM books", (error, rows) => {
+              rows.forEach((row) => console.log(`${row.id} ${row.title}`));
+            }),
+              () => {
+                db.run("DROP TABLE books");
+              };
+          },
+        );
+      },
+    );
+  },
 );
-
-await timers.setTimeout(100);
-db.run("INSERT INTO books (title) VALUES (?)", "ゼロからわかるRuby超入門");
-
-await timers.setTimeout(100);
-db.run(
-  "INSERT INTO books (title) VALUES (?)",
-  "プロを目指す人のためのRuby入門",
-);
-
-await timers.setTimeout(100);
-db.all("SELECT * FROM books", (error, rows) => {
-  rows.forEach((row) => console.log(`${row.id} ${row.title}`));
-});
-
-await timers.setTimeout(100);
-db.run("DROP TABLE books");

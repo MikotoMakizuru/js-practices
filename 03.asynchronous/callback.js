a7fd4bd435dad5d06b276d242d9139c786b13160
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-08 02:34:01 +0900

    SQL:CREATE TABLEのif文削除
    Prettierを通した
    
    自動採番されたIDを標準出力に出力
    
    sqlite3のライブラリのエラー修正

--- 03.asynchronous/callback.js
+++ 03.asynchronous/callback.js
@@ -3,17 +3,21 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
   () => {
     db.run(
       "INSERT INTO books (title) VALUES (?)",
       "ゼロからわかるRuby超入門",
-      () => {
+      function () {
+        const lastID = this.lastID;
+        console.log(`ID:${lastID}`);
         db.run(
           "INSERT INTO books (title) VALUES (?)",
           "プロを目指す人のためのRuby入門",
-          () => {
-            db.all("SELECT * FROM books", (error, rows) => {
+          function () {
+            const lastID = this.lastID;
+            console.log(`ID:${lastID}`);
+            db.all("SELECT * FROM books", (_error, rows) => {
               rows.forEach((row) => console.log(`${row.id} ${row.title}`));
             }),
               () => {

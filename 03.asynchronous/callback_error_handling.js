60ab9a903ee4784a7adf189b3368a484bd2ad61c
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-16 23:19:17 +0900

    テーブル作成

--- 03.asynchronous/callback_error_handling.js
+++ 03.asynchronous/callback_error_handling.js
@@ -1,41 +1,7 @@
 import sqlite3 from "sqlite3";
-import timers from "timers/promises";
 
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
 );
-
-await timers.setTimeout(100);
-db.run(
-  "INSERT INTO books (title) VALUES (?)",
-  "ゼロからわかるRuby超入門",
-  (error) => {
-    if (error) {
-      console.log("本のタイトル追加失敗");
-    }
-  },
-);
-
-await timers.setTimeout(100);
-db.run(
-  "INSERT INTO books (title) VALUES (?)",
-  "プロを目指す人のためのRuby入門",
-  (error) => {
-    if (error) {
-      console.log("本のタイトル追加失敗");
-    }
-  },
-);
-
-await timers.setTimeout(100);
-db.all("SELECT * FROM books", (error, rows) => {
-  if (error) {
-    console.log("本のタイトル表示失敗");
-  }
-  rows.forEach((row) => console.log(`${row.id} ${row.title}`));
-});
-
-await timers.setTimeout(100);
-db.run("DROP TABLE books");

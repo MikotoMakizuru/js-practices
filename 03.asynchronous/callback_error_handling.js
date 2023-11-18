0660195283978b05e8538536608c3de2b70b7c05
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-18 15:24:16 +0900

    エラーハンドリングありプログラム作成

--- 03.asynchronous/callback_error_handling.js
+++ 03.asynchronous/callback_error_handling.js
@@ -3,5 +3,22 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 db.run(
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  () => {
+    db.run(
+      "INSERT INTO book (title) VALUES (?)",
+      "JavaScript Primer 迷わないための入門",
+      (error) => {
+        if (error) {
+          console.log("追加", error.message);
+        }
+        db.all("SELECT * FROM booka", (error) => {
+          if (error) {
+            console.log("取得", error.message);
+            db.run("DROP TABLE books");
+          }
+        });
+      }
+    );
+  }
 );

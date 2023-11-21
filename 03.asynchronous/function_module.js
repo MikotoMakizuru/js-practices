802d95d716ad06553aa018bf427f3b873a901b96
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-21 23:44:18 +0900

    node-sqlite3の関数をPromiseベースにした関数作成

--- 03.asynchronous/function_module.js
+++ 03.asynchronous/function_module.js
@@ -2,81 +2,44 @@ import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database(":memory:");
 
-function createTable() {
-  return new Promise((resolve, reject) => {
-    db.run(
-      "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
-      function (error) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      },
-    );
+function createTable(createTableSql) {
+  return new Promise((resolve) => {
+    db.run(createTableSql, () => {
+      resolve();
+    });
   });
 }
 
-function insertBookTitleZeroRuby() {
+function insertRecord(insertRecordSql, booksTitle) {
   return new Promise((resolve, reject) => {
-    db.run(
-      "INSERT INTO books (title) VALUES (?)",
-      "ゼロからわかるRuby超入門",
-      function (error) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      },
-    );
-  });
-}
-
-function insertBookTitleCherryBook() {
-  return new Promise((resolve, reject) => {
-    db.run(
-      "INSERT INTO books (title) VALUES (?)",
-      "プロを目指す人のためのRuby入門",
-      function (error) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve();
-        }
-      },
-    );
-  });
-}
-
-function outputTitleDisplay() {
-  return new Promise((resolve, reject) => {
-    db.each("SELECT * FROM books", function (error, row) {
+    db.run(insertRecordSql, booksTitle, function (error) {
       if (error) {
         reject(error);
       } else {
-        resolve(console.log(`${row.id} ${row.title}`));
+        resolve(this.lastID);
       }
     });
   });
 }
 
-function deleteTable() {
+function selectData(selectDataSql) {
   return new Promise((resolve, reject) => {
-    db.run("DROP TABLE books", function (error) {
+    db.get(selectDataSql, function (error, rows) {
       if (error) {
         reject(error);
       } else {
-        resolve();
+        resolve(rows);
       }
     });
   });
 }
 
-export {
-  createTable,
-  insertBookTitleZeroRuby,
-  insertBookTitleCherryBook,
-  outputTitleDisplay,
-  deleteTable,
-};
+function deleteTable(deleteTableSql) {
+  return new Promise((resolve) => {
+    db.run(deleteTableSql, () => {
+      resolve();
+    });
+  });
+}
+
+export { createTable, insertRecord, selectData, deleteTable };

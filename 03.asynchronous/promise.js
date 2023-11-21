1966defe35b1e16da47aa1f1f42c45751a0078e8
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-21 23:44:47 +0900

    Promiseエラーハンドリングなしプログラム作成

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,8 +1,20 @@
 import * as db_operation_function from "./function_module.js";
 
+const createTableSql =
+  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
+const insertRecordSql = "INSERT INTO books (title) VALUES (?)";
+const booksTitle = "JavaScript Primer 迷わないための入門";
+const selectDataSql = "SELECT * FROM books";
+const deleteTableSql = "DROP TABLE books";
+
 db_operation_function
-  .createTable()
-  .then(() => db_operation_function.insertBookTitleZeroRuby())
-  .then(() => db_operation_function.insertBookTitleCherryBook())
-  .then(() => db_operation_function.outputTitleDisplay())
-  .then(() => db_operation_function.deleteTable());
+  .createTable(createTableSql)
+  .then(() => db_operation_function.insertRecord(insertRecordSql, booksTitle))
+  .then((lastID) => {
+    console.log(`ID: ${lastID}`);
+  })
+  .then(() => db_operation_function.selectData(selectDataSql))
+  .then((rows) => {
+    console.log(`${rows.id} ${rows.title}`);
+  })
+  .then(() => db_operation_function.deleteTable(deleteTableSql));

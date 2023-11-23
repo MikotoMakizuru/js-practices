1553c50b02af6cb9b200fbf7b1954ca0761403e5
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-23 12:26:26 +0900

    引数に直接SQLを書くように修正

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -1,20 +1,20 @@
 import * as db_operation_function from "./function_module.js";
 
-const createTableSql =
-  "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)";
-const insertRecordSql = "INSERT INTO books (title) VALUES (?)";
-const booksTitle = "JavaScript Primer 迷わないための入門";
-const selectDataSql = "SELECT * FROM books";
-const deleteTableSql = "DROP TABLE books";
-
 db_operation_function
-  .promiseRun(createTableSql)
-  .then(() => db_operation_function.promiseRun(insertRecordSql, booksTitle))
+  .promiseRun(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  )
+  .then(() =>
+    db_operation_function.promiseRun(
+      "INSERT INTO books (title) VALUES (?)",
+      "JavaScript Primer 迷わないための入門"
+    )
+  )
   .then((lastID) => {
     console.log(`ID: ${lastID}`);
   })
-  .then(() => db_operation_function.promiseAll(selectDataSql))
+  .then(() => db_operation_function.promiseAll("SELECT * FROM books"))
   .then((rows) => {
     rows.forEach((row) => console.log(`${row.id} ${row.title}`));
   })
-  .then(() => db_operation_function.promiseRun(deleteTableSql));
+  .then(() => db_operation_function.promiseRun("DROP TABLE books"));

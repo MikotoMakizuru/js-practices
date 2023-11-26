558f75de52907a53b1a836ce5602e09201f837d8
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2023-11-26 18:13:36 +0900

    async, await形式のエラーハンドリングありプログラム作成

--- 03.asynchronous/async-error-handling.js
+++ 03.asynchronous/async-error-handling.js
@@ -1,23 +1,28 @@
-import * as db_operation_function from "./function_module.js";
+import sqlite3 from "sqlite3";
+import { promiseRun, promiseAll } from "./db-operation.js";
+
+const db = new sqlite3.Database(":memory:");
 
 async function main() {
-  await db_operation_function.createTable();
+  await promiseRun(
+    db,
+    "CREATE TABLE books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)"
+  );
   try {
-    await db_operation_function.insertBookTitleZeroRuby();
+    await promiseRun(
+      db,
+      "INSERT INTO book (title) VALUES (?)",
+      "JavaScript Primer 迷わないための入門"
+    );
   } catch (error) {
-    console.log("本のタイトルの追加に失敗（ゼロからわかるRuby超入門）");
+    console.error("追加", error.message);
   }
   try {
-    await db_operation_function.insertBookTitleCherryBook();
-  } catch {
-    console.log("本のタイトルの追加に失敗（プロを目指す人のためのRuby入門）");
+    await promiseAll(db, "SELECT * FROM booka");
+  } catch (error) {
+    console.error("取得", error.message);
   }
-  try {
-    await db_operation_function.outputTitleDisplay();
-  } catch {
-    console.log("本のタイトルの表示に失敗");
-  }
-  await db_operation_function.deleteTable();
+  await promiseRun(db, "DROP TABLE books");
 }
 
 main();

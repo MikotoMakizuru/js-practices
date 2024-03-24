145cf9af1198eaa2dde1aa90a064e1bfd6880427
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-03-24 19:32:49 +0900

    クエリ修正

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -50,7 +50,7 @@ export default class Memo {
 
   select(id) {
     return new Promise((resolve, reject) => {
-      this.db.get("SELECT * FROM memos WHERE id == ?", [id], (err, rows) => {
+      this.db.get("SELECT * FROM memos WHERE id = ?", [id], (err, rows) => {
         if (!err) {
           resolve(rows);
         } else {

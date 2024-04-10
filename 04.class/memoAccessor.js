a2788d3461b4869a821e23962f738ab5dcb2887d
MikotoMakizuru <mikoto.makizuru3510@gmail.com>
2024-04-11 00:54:42 +0900

    メモの追加・メモ一覧の取得の処理が実行される前にcreateTableメソッドが実行されるようにした

--- 04.class/memoAccessor.js
+++ 04.class/memoAccessor.js
@@ -3,9 +3,6 @@ import sqlite3 from "sqlite3";
 export default class MemoAccessor {
   constructor() {
     this.db = new sqlite3.Database("./DB/memo.sqlite3");
-    this.db.serialize(() => {
-      this.createTable();
-    });
   }
 
   createTable() {
@@ -23,8 +20,9 @@ export default class MemoAccessor {
     });
   }
 
-  insert(title, content) {
-    return new Promise((resolve, reject) => {
+  async insert(title, content) {
+    await this.createTable();
+    return await new Promise((resolve, reject) => {
       this.db.run(
         "INSERT INTO memos (title, content) VALUES (?, ?)",
         [title, content],
@@ -39,8 +37,9 @@ export default class MemoAccessor {
     });
   }
 
-  selectAll() {
-    return new Promise((resolve, reject) => {
+  async selectAll() {
+    await this.createTable();
+    return await new Promise((resolve, reject) => {
       this.db.all("SELECT * FROM memos ORDER BY id", (err, memos) => {
         if (!err) {
           resolve(memos);
